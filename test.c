8d255792bf2b5426c77d39a915ab7f84d39b017f
liigo <com.liigo@gmail.com>
2013-11-01 18:48:58 +0800

    apply asserts in test units

--- test.c
+++ test.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <assert.h>
 
 void writefile(const char* data, const char* file)
 {
@@ -25,7 +26,7 @@ int main()
 	test("Liigo%E9%AB%98%E6%89%8B", "%utf8", "2.txt");
 	// "Liigo是老大 于 论坛上 求贴解 都被删掉  齉爨"
 	test("Liigo%u662F%u8001%u5927 %u4e8e %u8bba%u575B%u4e0A %u6c42%u8d34%u89e3 %u90Fd%u88aB%u5220%u6389  %u9f49%u7228", "%u", "3.txt");
-	// 龍×4, 敞+鱼+电（上下结构，鱼无横）（这两个都是非常复杂的汉字，是Unicode标准后来新加入的，未必有字体支持其显示）
+	// 龍×4, 敞+鱼+电（上中下结构，鱼无横）（这两个都是非常复杂的汉字，是Unicode标准后来新加入的，未必有字体支持其显示）
 	test("Liigo%U0002A6A5%U0002B81D", "%u", "4.txt");
 
 	{
@@ -34,19 +35,19 @@ int main()
 		unsigned char utf8[] = { 0xe6,0x98,0xaf, 0xe8,0x80,0x81, 0xe5,0xa4,0xa7, // 是老大
 						0xe4,0xba,0x8e, 0xe8,0xae,0xba, 0xe5,0x9d,0x9b, // 于论坛
 						0xe9,0xbd,0x89, 0xe7,0x88,0xa8, // 齉爨
-						0xF0,0xAA,0x9A,0xA5, 0xF0,0xAB,0xA0,0x9D, // 龍×4, 敞+鱼+电（上下结构，鱼无横），每字UTF-8编码需4字节
+						0xF0,0xAA,0x9A,0xA5, 0xF0,0xAB,0xA0,0x9D, // 龍×4, 敞+鱼+电（上中下结构，鱼无横），每字UTF-8编码需4字节
 						0x00, };
 
-		codepoint = UTF8_to_Codepoint(utf8 + 0, &bytes); // codepoint: 0x662F, bytes: 3
-		codepoint = UTF8_to_Codepoint(utf8 + 3, &bytes); // codepoint: 0x8001, bytes: 3
-		codepoint = UTF8_to_Codepoint(utf8 + 6, &bytes); // codepoint: 0x5927, bytes: 3
-		codepoint = UTF8_to_Codepoint(utf8 + 9, &bytes); // codepoint: 0x4e8e, bytes: 3
-		codepoint = UTF8_to_Codepoint(utf8 +12, &bytes); // codepoint: 0x8bba, bytes: 3
-		codepoint = UTF8_to_Codepoint(utf8 +15, &bytes); // codepoint: 0x575b, bytes: 3
-		codepoint = UTF8_to_Codepoint(utf8 +18, &bytes); // codepoint: 0x9f49, bytes: 3
-		codepoint = UTF8_to_Codepoint(utf8 +21, &bytes); // codepoint: 0x7228, bytes: 3
-		codepoint = UTF8_to_Codepoint(utf8 +24, &bytes); // codepoint: 0x0002A6A5, bytes: 4
-		codepoint = UTF8_to_Codepoint(utf8 +28, &bytes); // codepoint: 0x0002B81D, bytes: 4
+		codepoint = UTF8_to_Codepoint(utf8 + 0, &bytes); assert(codepoint==0x662F && bytes==3); // 是
+		codepoint = UTF8_to_Codepoint(utf8 + 3, &bytes); assert(codepoint==0x8001 && bytes==3); // 老
+		codepoint = UTF8_to_Codepoint(utf8 + 6, &bytes); assert(codepoint==0x5927 && bytes==3); // 大
+		codepoint = UTF8_to_Codepoint(utf8 + 9, &bytes); assert(codepoint==0x4e8e && bytes==3); // 于
+		codepoint = UTF8_to_Codepoint(utf8 +12, &bytes); assert(codepoint==0x8bba && bytes==3); // 论
+		codepoint = UTF8_to_Codepoint(utf8 +15, &bytes); assert(codepoint==0x575b && bytes==3); // 坛
+		codepoint = UTF8_to_Codepoint(utf8 +18, &bytes); assert(codepoint==0x9f49 && bytes==3); // 齉
+		codepoint = UTF8_to_Codepoint(utf8 +21, &bytes); assert(codepoint==0x7228 && bytes==3); // 爨
+		codepoint = UTF8_to_Codepoint(utf8 +24, &bytes); assert(codepoint==0x0002A6A5 && bytes==4); // 龍×4
+		codepoint = UTF8_to_Codepoint(utf8 +28, &bytes); assert(codepoint==0x0002B81D && bytes==4); // 敞+鱼+电（上中下结构，鱼无横）
 		codepoint = 0;
 	}
 }
